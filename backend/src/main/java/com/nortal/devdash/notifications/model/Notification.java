8fecfd5d5539f7c6885655472fb6b00682dcb310
Margus Hanni <margush@hot.ee>
2016-12-11 19:46:29 +0200

    Added Swagger 2, updated project structure

--- backend/src/main/java/com/nortal/devdash/notifications/model/Notification.java
+++ backend/src/main/java/com/nortal/devdash/notifications/model/Notification.java
@@ -4,14 +4,22 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 @Entity
+@ApiModel(value = "Notification model object")
 public class Notification {
 
     @Id
     @GeneratedValue
+    @ApiModelProperty(value = "Notification identificator")
     private Long id;
+    @ApiModelProperty(value = "Notification text")
     private String text;
+    @ApiModelProperty(value = "Notification type")
+    private NotificationType type;
+
 }
